0b48863c67168403f67828c1c25ecde406c1957c
nullone <adviventes@gmail.com>
2021-06-10 04:13:09 +0300

    Added swagger documentation support

--- WebMessages/Startup.cs
+++ WebMessages/Startup.cs
@@ -5,43 +5,57 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace WebMessages
 {
+    /// <summary>
+    /// Class to set up api options.
+    /// </summary>
     public class Startup
     {
-        public Startup(IConfiguration configuration) {
+        /// <summary>
+        /// Constructor with configuration argument.
+        /// </summary>
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
+        /// <summary>
+        /// General configuration.
+        /// </summary>
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
+        /// <summary>
+        /// Configuration of services.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddControllers();
-            
-            // Register the Swagger generator, defining 1 or more Swagger documents
-            services.AddSwaggerGen();
+
+            // Adding XML-documentation swagger possibility.
+            services.AddSwaggerGen(c =>
+            {
+                var filePath = Path.Combine(System.AppContext.BaseDirectory, "WebMessages.xml");
+                c.IncludeXmlComments(filePath);
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        /// <summary>
+        /// Configuration of HTTP pipeline.
+        /// </summary>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
             // specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
+
+            if (env.IsDevelopment())
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
-            
-            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
@@ -51,10 +65,7 @@ namespace WebMessages
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
